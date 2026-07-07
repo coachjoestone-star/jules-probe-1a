ec98c37a285258614dd67da853d81303b09c2759
coachjoestone-star <coachjoestone@gmail.com>
2026-07-07 15:29:37 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+// Minimal LRU cache
+class LRUCache {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.map = new Map();
+  }
+  get(key) {
+    if (!this.map.has(key)) return undefined;
+    const val = this.map.get(key);
+    this.map.delete(key);
+    this.map.set(key, val);
+    return val;
+  }
+  set(key, value) {
+    if (this.map.has(key)) this.map.delete(key);
+    this.map.set(key, value);
+    if (this.map.size > this.capacity) {
+      this.map.delete(this.map.keys().next().value);
+    }
+  }
+}
+module.exports = LRUCache;
